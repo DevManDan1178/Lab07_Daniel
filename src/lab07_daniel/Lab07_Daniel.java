2ff63556fce4e971501a4955767ba3951d86a6c7
Daniel Orejuela Liu <6323612@edu.vaniercollege.qc.ca>
2025-10-20 09:41:40 -0400

    added rectangle

--- src/lab07_daniel/Lab07_Daniel.java
+++ src/lab07_daniel/Lab07_Daniel.java
@@ -4,7 +4,12 @@
  */
 package lab07_daniel;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +27,19 @@ public class Lab07_Daniel extends Application{
 
     @Override
     public void start(Stage stage) {
+        final int SCENE_WIDTH = 500, SCENE_LENGTH = 500;
+        
+        BorderPane root = new BorderPane();
+        
+        Rectangle pathRect = new Rectangle(0, 0, SCENE_WIDTH, SCENE_LENGTH);
+        pathRect.setFill(null);
+        pathRect.setStroke(Color.DARKBLUE);
+        
+
+        root.getChildren().add(pathRect);
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_LENGTH);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
