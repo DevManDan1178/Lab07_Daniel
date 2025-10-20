c4217840be5d2f6da0b16ffcc6224fc939c1a185
Daniel Orejuela Liu <6323612@edu.vaniercollege.qc.ca>
2025-10-20 09:33:42 -0400

    first commit

---
+++ src/lab07_daniel/Lab07_Daniel.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab07_daniel;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6323612
+ */
+public class Lab07_Daniel extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    }
+    
+}
